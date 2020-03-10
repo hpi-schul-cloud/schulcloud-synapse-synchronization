4d674121b0b669e248321ab4ad23d06d9c2b6f10
Jan Renz <jan.renz@gmail.com>
2020-03-10 21:31:47 +0100

    starting to structure the code

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ var testObj = {
     },
     rooms:[
         {
-            id: '12345ssss6s61ddd2dd212',
+            id: '12345sssdds6s61ddd2dd212',
             name: 'Accouncement Test',
             type: 'course',
             announcements_only: true,
             is_moderator: true 
         },
         {
-            id: 'sdgdfsgeeffewsdfg',
+            id: 'sdgdfsgeeddffewsdfg',
             name: 'Kurs 5 (bidirektional)',
             type: 'course',
             announcements_only: false,
@@ -119,45 +119,7 @@ async function syncUserWithMatrix(payload){
     asyncForEach(payload.rooms, async (room) => {
         let alias = room.type + "_" + room.id;
         var fq_alias = "%23" + alias + ":" + MATRIX_DOMAIN;
-        let room_matrix_id = null;
-        let room_already_present = false;
-        // check if exists and permissions levels are what we want
-        // GET /_matrix/client/r0/directory/room/{roomAlias}
-        console.log("check room " + alias);
-        await matrix_admin_api.get('/_matrix/client/r0/directory/room/' + fq_alias)
-        .then(function (response) {
-            if (response.status == 200){
-                room_already_present = true;
-                room_matrix_id = response.data.room_id;
-                console.log("room " + room_matrix_id + " found");
-            }
-        })
-        .catch(function (error) {
-            console.log("room " + alias + " not found");
-        })
-        // TODO: Update room title if needed
-        //create room
-        if (room_already_present == false){
-            await matrix_admin_api.post('/_matrix/client/r0/createRoom', {
-                "preset": "private_chat", // this allows guest, we might want to disallow this later
-                "room_alias_name": alias,
-                "name": room.name,
-                "topic": "Kanal für " + room.name + " (" + payload.school.name + ")",
-                "creation_content": {
-                }
-            })
-            .then(function (response) {
-                if (response.status == 200){
-                    room_already_present = true;
-                    room_matrix_id = response.data.room_id;
-                    console.log("room " + room_matrix_id + " created");
-                }
-            })
-            .catch(function (error) {
-                console.log("room " + alias + " not found");
-              }
-            )
-        }
+        let room_matrix_id = await createRoom(fq_alias, alias, room.name, payload.school.name);
 
         //join user
        // note that we need to encode the #
@@ -179,28 +141,10 @@ async function syncUserWithMatrix(payload){
         if (room.announcements_only == true){
             desiredUserPower = 50;
         }
-        var room_state = {};
-        await matrix_admin_api.get('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels').then(function (response) {
-            if (response.status == 200){
-                room_state = response.data;
-                console.log(response.data);
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-          }
-        )
-        if (room_state.events_default != desiredUserPower){
-            room_state.events_default = desiredUserPower;
-            await matrix_admin_api.put('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels', room_state)
-            .then(function (response) {
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            }
-            )
-        }
+        // this can run async
+        setRoomEventsDefault(room_matrix_id, desiredUserPower);
+
+
         // check moderator
         await matrix_admin_api.get('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels').then(function (response) {
             if (response.status == 200){
@@ -259,4 +203,71 @@ async function syncUserWithMatrix(payload){
      }
 };
 
+// check if room exists, if not create
+async function setRoomEventsDefault(room_matrix_id, events_default){
+    var room_state = {};
+    await matrix_admin_api.get('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels').then(function (response) {
+        if (response.status == 200){
+            room_state = response.data;
+            console.log(response.data);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+      }
+    )
+    if (room_state.events_default != events_default){
+        room_state.events_default = events_default;
+        await matrix_admin_api.put('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels', room_state)
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        }
+        )
+    }
+}
+
+// returns room_matrix_id
+
+async function createRoom(fq_alias, alias, room_name, school_name){
+    let room_already_present = false;
+    console.log("check room " + fq_alias);
+        await matrix_admin_api.get('/_matrix/client/r0/directory/room/' + fq_alias)
+        .then(function (response) {
+            if (response.status == 200){
+                room_already_present = true;
+                room_matrix_id = response.data.room_id;
+                console.log("room " + room_matrix_id + " found");
+            }
+        })
+        .catch(function (error) {
+            console.log("room " + fq_alias + " not found");
+        })
+        // TODO: Update room title if needed
+        //create room
+        if (room_already_present == false){
+            await matrix_admin_api.post('/_matrix/client/r0/createRoom', {
+                "preset": "private_chat", // this allows guest, we might want to disallow this later
+                "room_alias_name": alias,
+                "name": room_name,
+                "topic": "Kanal für " + room_name + " (" + school_name + ")",
+                "creation_content": {
+                }
+            })
+            .then(function (response) {
+                if (response.status == 200){
+                    room_already_present = true;
+                    room_matrix_id = response.data.room_id;
+                    console.log("room " + room_matrix_id + " created");
+                }
+            })
+            .catch(function (error) {
+                console.log("room " + alias + " not found");
+              }
+            )
+        }
+        return room_matrix_id;
+}
 syncUserWithMatrix(testObj);
