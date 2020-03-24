c65f6e28f3ee0697c21f3b6d08d84fa9f3033c60
Max Klenk <max@klenk.biz>
2020-03-24 17:41:57 +0100

    handle throtteling and wait until full msg is finished

--- src/syncer.js
+++ src/syncer.js
@@ -5,11 +5,43 @@ const axios = require('axios');
 const MATRIX_DOMAIN = Configuration.get("MATRIX_DOMAIN");
 const MATRIX_SYNC_USER_TOKEN = secrets.matrix_sync_user_token;
 
-const matrix_admin_api = axios.create({
-  baseURL: "https://" + MATRIX_DOMAIN,
+const axios_matrix_admin_api = axios.create({
+  baseURL: 'https://' + MATRIX_DOMAIN,
   timeout: 10000,
   headers: {'Authorization': 'Bearer ' + MATRIX_SYNC_USER_TOKEN}
 });
+
+const matrix_admin_api = {
+  get: (first, second, third = null) => {
+    return call('get', first, second, third);
+  },
+  put: (first, second, third) => {
+    return call('put', first, second, third);
+  },
+  post: (first, second, third) => {
+    return call('post', first, second, third);
+  },
+};
+
+function call(func, first, second, third) {
+  return axios_matrix_admin_api[func](first, second, third)
+    .catch(async (error) => {
+      if (error.response.status === 429) {
+        const timeToWait = error.response.data.retry_after_ms + 100;
+        console.warn(`Waiting for ${timeToWait}ms...`);
+        await sleep(timeToWait);
+        return matrix_admin_api[func](first, second, third);
+      } else {
+        throw error
+      }
+    });
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 module.exports = {
   syncUserWithMatrix: syncUserWithMatrix,
@@ -46,7 +78,7 @@ async function syncUserWithMatrix(payload) {
   }
 
   if (payload.rooms) {
-    asyncForEach(payload.rooms, async (room) => {
+    await asyncForEach(payload.rooms, async (room) => {
       const alias = room.type + "_" + room.id;
       const fq_alias = "%23" + alias + ":" + MATRIX_DOMAIN;
       let [room_matrix_id] = await createRoom(fq_alias, alias, room.name, payload.school.name);
@@ -57,8 +89,8 @@ async function syncUserWithMatrix(payload) {
         desiredUserPower = 0;
       }
       // this can run async
-      setRoomEventsDefault(room_matrix_id, desiredUserPower);
-      setModerator(room_matrix_id, payload.user.id, room.is_moderator);
+      await setRoomEventsDefault(room_matrix_id, desiredUserPower);
+      await setModerator(room_matrix_id, payload.user.id, room.is_moderator);
     });
   }
 
@@ -69,10 +101,10 @@ async function syncUserWithMatrix(payload) {
     let alias = "news_" + payload.school.id;
     const fq_alias = "%23" + alias + ":" + MATRIX_DOMAIN;
     const [room_matrix_id, current_permission] = await createRoom(fq_alias, alias, room_name, payload.school.name, topic, payload.user.is_school_admin ? payload.user.id : null);
-    setRoomEventsDefault(room_matrix_id, 50);
+    await setRoomEventsDefault(room_matrix_id, 50);
     await joinUserToRoom(user_id, room_matrix_id);
     if (payload.user.is_school_admin && current_permission !== 50) {
-      setModerator(room_matrix_id, payload.user.id, true);
+      await setModerator(room_matrix_id, payload.user.id, true);
     }
   } else {
     //TODO: delete or block room if setting is changed
@@ -88,7 +120,7 @@ async function syncUserWithMatrix(payload) {
     // setRoomEventsDefault(room_matrix_id, 50);
     await joinUserToRoom(user_id, room_matrix_id);
     if (payload.user.is_school_admin && current_permission !== 50) {
-      setModerator(room_matrix_id, payload.user.id, true);
+      await setModerator(room_matrix_id, payload.user.id, true);
     }
   }
 }
@@ -185,26 +217,26 @@ async function createRoom(fq_alias, alias, room_name, school_name, topic = null,
       );
     const tmp_state = await getRoomState(room_matrix_id);
     tmp_state.invite = 70;
-    setRoomState(room_matrix_id, tmp_state);
+    await setRoomState(room_matrix_id, tmp_state);
   }
   // user_permission_
   return [room_matrix_id, current_user_level];
 }
 
 async function getRoomState(room_matrix_id) {
-  let data = null;
-  await matrix_admin_api.get('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels').then(function(response) {
+  return matrix_admin_api.get('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels').then(function(response) {
     if (response.status === 200) {
-      data = response.data;
+      return response.data;
     }
+    return null;
   }).catch(logRequestError);
-  return data;
 }
 
 async function setRoomState(room_matrix_id, room_state) {
-  return await matrix_admin_api.put('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels', room_state)
+  return matrix_admin_api.put('/_matrix/client/r0/rooms/' + room_matrix_id + '/state/m.room.power_levels', room_state)
     .then(function(_) {
       console.log("set roomm state in " + room_matrix_id);
+      return true;
     })
     .catch(logRequestError);
 }
@@ -238,5 +270,5 @@ async function asyncForEach(array, callback) {
 
 
 function logRequestError(error) {
-  console.error(error.response.status);
+  console.error(error.response);
 }
